19bb94a57c56f049069d37487f709724976dd638
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2020-12-07 22:28:26 -0800

    working commit - Created using LeetHub

---
+++ path-sum/path-sum.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        stack = [(root, root.val)]
+        while stack:
+            curr, val = stack.pop()
+            if not curr.left and not curr.right and val == sum:
+                return True
+            if curr.right:
+                stack.append((curr.right, val+curr.right.val))
+            if curr.left:
+                stack.append((curr.left, val+curr.left.val))
+        return False
+​
+        
+  

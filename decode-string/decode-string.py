476daf07f4048dbca38b3b493338e21e2113d442
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-02-02 22:08:06 -0800

    Time: 56 ms (6.46%), Space: 14.2 MB (81.93%) - LeetHub

--- decode-string/decode-string.py
+++ decode-string/decode-string.py
@@ -1,26 +1,23 @@
 class Solution:
-    def decodeString(self, s: str) -> str:
-        currentString = ""
-        k = 0
-        stack = []
-        
-        for char in s:
-            
-            if char == '[' :
-                stack.append((currentString, k))
-                currentString = ""
-                k = 0
-            
-            elif char == ']' :
-                last_string, last_k = stack.pop(-1)
-                currentString = last_string + last_k * currentString
-                
-            elif char.isdigit() :
-                k = k * 10 + int(char)
-                
-            else:
-                currentString += char
-                
-        
-        return currentString
-        
+    def decodeString(self, s: str) -> str:
+        stack = []
+        curr_string = ''
+        k = 0
+        
+        for i in s:
+            if i == '[':
+                stack.append((curr_string, k))
+                curr_string, k = '', 0
+                
+            elif i == ']':
+                last_string, last_k = stack.pop()
+                curr_string = last_string + last_k * curr_string
+                
+            elif i.isdigit():
+                k = k * 10 + int(i)
+            
+            else:
+                curr_string += i
+                
+        
+        return curr_string

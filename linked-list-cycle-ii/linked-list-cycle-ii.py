f7ab22e5e13632e44e471664801f7d18dd083160
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-05-07 23:52:26 -0700

    Time: 52 ms (52.91%), Space: 17.1 MB (80.17%) - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.py
+++ linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -1,27 +1,28 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-​
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
-        marker1 = head
-        marker2 = head
-        
-        while marker2 and marker2.next:
-            marker1 = marker1.next
-            marker2 = marker2.next.next
-            
-            if marker1 == marker2:
-                break
-        else:
-            
-            return None
-        
-        while head != marker1:
-            head = head.next
-            marker1 = marker1.next
-            
-        return head
-        
+    def detectCycle(self, head: ListNode) -> ListNode:
+        marker1 = marker2 = head
+        
+        while marker2 and marker2.next:
+            marker1 = marker1.next
+            marker2 = marker2.next.next
+            
+            if marker1 == marker2:
+                break
+        else:
+            
+            return None
+        
+        
+        while head != marker1:
+            head = head.next
+            marker1 = marker1.next
+            
+        
+        return head
+            

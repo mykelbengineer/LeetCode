3db05ea565e6699d24a42834e253e9f94ea3cbd1
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-05-12 23:24:02 -0700

    Time: 36 ms (9.55%), Space: 14.3 MB (89.05%) - LeetHub

--- reverse-linked-list-ii/reverse-linked-list-ii.py
+++ reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -1,29 +1,30 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
-        
-        if not head or m == n: return head
-        
-        prev = dummy = ListNode(None)
-        dummy.next = head
-        curr = head
-        
-        for _ in range(m-1):
-            prev = prev.next
-            curr = curr.next
-            
-        for _ in range(n-m):
-            tmp = curr.next
-            curr.next = tmp.next
-            tmp.next = prev.next
-            prev.next = tmp
-            
-        
-        return dummy.next
-  
-        
-        
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        if not head or m == n: return head
+        
+        prev = dummy = ListNode(None)
+        dummy.next = head
+        curr = head
+        
+        
+        for _ in range(m-1):
+            prev = prev.next
+            curr = curr.next
+            
+        for _ in range(n-m):
+            tmp = curr.next
+            curr.next = tmp.next
+            tmp.next = prev.next
+            prev.next = tmp
+            
+        return dummy.next
+        
+        
+        
+        
+        

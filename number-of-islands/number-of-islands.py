25ab6c171a5fa6829cd149e136d2842c541b527f
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-05-16 20:58:06 -0700

    Time: 132 ms (85.47%), Space: 15.5 MB (35.41%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,15 +1,16 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        
         count = 0
         
         def dfs(grid, i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
-                return 0
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+                return 
             
             grid[i][j] = '0'
             
-            dfs(grid, i-1, j)
-            dfs(grid, i+1, j)
+            dfs(grid, i + 1, j)
+            dfs(grid, i - 1, j)
             dfs(grid, i, j-1)
             dfs(grid, i, j+1)
             
@@ -20,4 +21,5 @@ class Solution:
                     dfs(grid, i, j)
                     count += 1
                     
+        
         return count

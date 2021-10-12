044102830c0a1a7eb94a110638987d83fcf55d95
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-10-11 20:17:45 -0700

    Time: 294 ms (5.37%), Space: 16.8 MB (51.45%) - LeetHub

--- max-area-of-island/max-area-of-island.py
+++ max-area-of-island/max-area-of-island.py
@@ -1,28 +1,26 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
         count = 0
+                            
+                    
+        def dfs(grid, i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] != 1:
+                return 0
+            
+            grid[i][j] = 0
+            
+            up = dfs(grid,i+1,j)
+            left = dfs(grid,i,j-1)
+            right = dfs(grid,i,j+1)
+            down = dfs(grid,i-1,j)
+            
+            return up + left + right + down + 1
         
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    count = max(count,self.dfs(grid, i, j))
-                    
+                    count = max(count, dfs(grid, i,j))
+
         return count
-    
-    
-    def dfs(self, grid, i, j):
-        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] == 0:
-            return 0
-        
-        grid[i][j] = 0
-        
-        up = self.dfs(grid, i-1,j)
-        down = self.dfs(grid, i+1,j)
-        left = self.dfs(grid, i,j-1)
-        right = self.dfs(grid, i,j+1)
-        
-        return 1 + up + down + left + right
         
         
-                    

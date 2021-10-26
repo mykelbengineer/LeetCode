278d4ef91007c5b7f1f46be63308b91cfc758c7c
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-10-25 20:44:02 -0700

    Time: 114 ms (16.26%), Space: 14.4 MB (11.99%) - LeetHub

--- add-two-numbers/add-two-numbers.py
+++ add-two-numbers/add-two-numbers.py
@@ -1,32 +1,27 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        
-        cur = dummyNode = ListNode(None)
-        carry = 0
-        
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-            
-            cur_val = v1 + v2 + carry
-            
-            carry, cur_val = divmod(cur_val, 10)
-            
-            cur.next = ListNode(cur_val)
-            
-            cur = cur.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            
-        
-        return dummyNode.next
-                
-                
-        
-        
-            
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1 and l2 : return None
+        
+        curr = dummy = ListNode(None)
+        carry = 0
+        
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            val = v1 + v2 + carry
+            
+            carry, val = divmod(val, 10)
+            curr.next = ListNode(val)
+            
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+                
+        return dummy.next
+        

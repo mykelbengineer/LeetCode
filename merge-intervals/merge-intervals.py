9ea602507a5e8e489b2b552e3039f4da9c4f604f
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-08 21:14:50 -0800

    Time: 80 ms (93.27%), Space: 16.2 MB (55.50%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,15 +1,28 @@
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key = lambda x:x[0])
-        
-        merged = []
-        
-        for i in intervals:
-            
-            if not merged or merged[-1][-1] < i[0]:
-                merged.append(i)
-            else:
-                merged[-1][-1] = max(merged[-1][-1], i[-1])
-                
-        return merged
-        
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted_intervals = sorted(intervals, key = lambda x:x[0])
+        output = []
+        
+        last_seen_start = sorted_intervals[0][0]
+        last_seen_end = sorted_intervals[0][1]
+        
+        for i in range(1, len(sorted_intervals)):
+            start = sorted_intervals[i][0]
+            end = sorted_intervals[i][1]
+            
+            if start <= last_seen_end:
+                last_seen_end = max(end, last_seen_end)
+                continue
+                
+            else:
+                output.append(list((last_seen_start, last_seen_end)))
+                last_seen_start = start
+                last_seen_end = end
+        output.append(list((last_seen_start, last_seen_end)))     
+        return output
+                
+                
+                
+        
+        
+        

dfac890855cf5da6576be927fcdf647bf3076aab
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-10 23:43:30 -0800

    Time: 92 ms (76.57%), Space: 15.5 MB (69.79%) - LeetHub

--- graph-valid-tree/graph-valid-tree.py
+++ graph-valid-tree/graph-valid-tree.py
@@ -1,27 +1,48 @@
-class Solution:
-    def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) != n - 1:
+class UnionFind:
+    def __init__(self,size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, idx):
+        if self.root[idx] == idx:
+            return idx
+        self.root[idx] = self.find(self.root[idx])
+        return self.root[idx]
+        
+    def union(self, idx, idy):
+        rootX = self.find(idx)
+        rootY = self.find(idy)
+        if rootX == rootY:
             return False
         
-        
-        adjList = [[] for _ in range(n)]
-        
-        for e1,e2 in edges:
-            adjList[e1].append(e2)
-            adjList[e2].append(e1)
-        
-        
-        stack = [0]
-        seen = {0}
-        
-        while stack:
-            node = stack.pop()
-            
-            for nei in adjList[node]:
-                if nei in seen:
-                    continue
-                seen.add(nei)
-                stack.append(nei)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
                 
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+                
+        return True
+    
+    def isConnected(self, idx, idy):
+        return self.find(idx) == self.find(idy)
+    
+            
         
-        return len(seen) == n
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1: return False
+        
+        UnionFinda = UnionFind(n)
+        
+        for e1, e2 in edges:
+            if not UnionFinda.union(e1,e2):
+                return False
+            
+        return True
+            
+        

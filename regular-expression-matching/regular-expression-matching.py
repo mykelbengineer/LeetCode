ceb1ad0c66c1f175ee40c0b4c219687f587cf30b
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-19 11:39:00 -0800

    Time: 36 ms (96.28%), Space: 14.7 MB (6.06%) - LeetHub

--- regular-expression-matching/regular-expression-matching.py
+++ regular-expression-matching/regular-expression-matching.py
@@ -1,33 +1,27 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        
         cache = {}
         
-        def back_tracking(i, j):
-            if (i,j) in cache:
-                return cache[(i,j)]
-            
+        def backTracking(i, j):
+            if (i,j) in cache: return cache[(i,j)]
             
             if i >= len(s) and j >= len(p): return True
-            if j >= len(p) : return False
+            if j >= len(p): return False
             
             match = i < len(s) and (s[i] == p[j] or p[j] == '.')
             
-            if (j+1) < len(p) and p[j+1] == '*':
-                cache[(i,j)] = back_tracking(i, j+2) or (match and back_tracking(i+1, j))
+            if j + 1 < len(p) and p[j+1] == '*':
+                cache[(i,j)] = backTracking(i, j+2) or (match and backTracking(i+1, j))
                 return cache[(i,j)]
-            
-            
+                
+                
             if match:
-                cache[(i,j)] = back_tracking(i+1, j+1)
+                cache[(i,j)] = backTracking(i+1, j+1)
                 return cache[(i,j)]
-            
-            
+                
+                
             cache[(i,j)] = False
-            return False
+            return cache[(i,j)]
         
         
-        
-        return back_tracking(0,0)
-            
-        
+        return backTracking(0,0)

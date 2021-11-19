ee40425526feb1f2212c529bfe98ec022658c72f
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-19 10:49:27 -0800

    Time: 120 ms (51.58%), Space: 17.8 MB (81.74%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.py
+++ merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,28 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        if not lists or len(lists) == 0:
-            return None
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: return None
         
         
         while len(lists) > 1:
-            sortedLists = []      
-            
+            sortedList = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
-                l2 = lists[i+1] if (i+1) < len(lists) else None
-                sortedLists.append(self.merge(l1, l2))      
-            lists = sortedLists
-        
+                l2 = lists[i+1] if i+1 < len(lists) else None
+                sortedList.append(self.merge(l1, l2))
+                
+            lists = sortedList
+            
         return lists[0]
-        
         
         
     
     def merge(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
+        tail = dummy = ListNode(None)
         
         while l1 and l2:
             if l1.val < l2.val:
@@ -34,12 +31,13 @@ class Solution:
                 
             else:
                 tail.next = l2
-                l2 = l2.next
+                l2= l2.next
                 
             tail = tail.next
             
         if l1: tail.next = l1
         if l2: tail.next = l2
+            
             
         return dummy.next
         

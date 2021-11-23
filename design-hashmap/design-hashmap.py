f94fcccadc1bd97baf08f366bad754b4d52f004e
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-22 19:11:48 -0800

    Time: 400 ms (32.90%), Space: 18 MB (27.17%) - LeetHub

--- design-hashmap/design-hashmap.py
+++ design-hashmap/design-hashmap.py
@@ -1,64 +1,45 @@
 class Node:
-    
     def __init__(self, key=-1, value=-1, next=None):
         self.key = key
-        self.value = value
-        self.next = next
+        self.value=value
+        self.next=None
 
 class MyHashMap:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.data = [Node() for _ in range(2069)]
         self.size = len(self.data)
         
-    def get_hash_code(self, key):
-        """
-        Returns hash value
-        """
+    def _getHashCode(self, key):
         return key % self.size
         
 
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
-        hashcode = self.get_hash_code(key)
+        hashcode = self._getHashCode(key)
         head = self.data[hashcode]
         
         while head.next:
-            if key == head.next.key:
+            if head.next.key == key:
                 head.next.value = value
                 return
-            
             head = head.next
             
-        head.next = Node(key, value)
-            
-        
+        head.next = Node(key,value)
 
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
-        hashcode = self.get_hash_code(key)
+        hashcode = self._getHashCode(key)
         head = self.data[hashcode]
         
         while head.next:
-            if key == head.next.key:
+            if head.next.key == key:
                 return head.next.value
+            
             head = head.next
             
         return -1
 
-
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
-        hashcode = self.get_hash_code(key)
+        hashcode = self._getHashCode(key)
         head = self.data[hashcode]
         
         while head.next:
@@ -68,6 +49,8 @@ class MyHashMap:
                 toRemove.next = None
                 return
             head = head.next
+        
+        
 
 
 # Your MyHashMap object will be instantiated and called as such:

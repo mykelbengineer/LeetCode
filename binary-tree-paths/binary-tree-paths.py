44a289aaf864ec4435a67071378bd4eb4a2cebde
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-23 20:14:54 -0800

    Time: 32 ms (73.67%), Space: 14.4 MB (31.12%) - LeetHub

--- binary-tree-paths/binary-tree-paths.py
+++ binary-tree-paths/binary-tree-paths.py
@@ -1,27 +1,29 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def binaryTreePaths(self, root: TreeNode) -> List[str]:
-        if not root: return []
-        
-        res = [] 
-        stack = [('', root)]
-        
-        while stack:
-            ls, node = stack.pop()
-            if not node.left and not node.right:
-                res.append(ls + str(node.val))
-            
-            if node.left:
-                stack.append((ls + str(node.val) + '->', node.left))
-                
-            if node.right:
-                stack.append((ls + str(node.val) + '->', node.right))
-                
-        return res
-            
-                
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root: return None
+        
+        paths = []
+        stack = [(root, str(root.val))]
+        
+        while stack:
+            node, path = stack.pop()
+            
+            if not node.left and not node.right:
+                paths.append(path)
+                
+            if node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+                
+            if node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
+                
+        
+        return paths
+                
+        

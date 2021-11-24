a3910e7822e6d306ead41a2fddb98e5718cff007
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-24 13:12:10 -0800

    Time: 45 ms (19.49%), Space: 14.3 MB (36.71%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,17 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        pa_map = {']':'[', '}':'{', ')':'('}
+        paren_map = {'}':'{', ']':'[', ')':'('}
+        
         stack = []
         
-        for brace in s:
-            if brace in pa_map:
-                last_elem = stack.pop() if stack else '#'
-                
-                if pa_map[brace] != last_elem:
+        for i in s:
+            if i in paren_map and stack:
+                if stack.pop() != paren_map[i]:
                     return False
-            
+                
             else:
-                stack.append(brace)
+                stack.append(i)
                 
         return not stack
         

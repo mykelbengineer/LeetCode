b3571cf4e51b200b20cfd6554a75003610354a3f
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-26 13:14:08 -0800

    Time: 70 ms (5.00%), Space: 14.4 MB (34.95%) - LeetHub

--- longest-happy-string/longest-happy-string.py
+++ longest-happy-string/longest-happy-string.py
@@ -1,38 +1,38 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        count_chars = []
-        if a:
-            heapq.heappush(count_chars, (-a, 'a'))
-        if b:
-            heapq.heappush(count_chars, (-b, 'b'))
-        if c:
-            heapq.heappush(count_chars, (-c, 'c'))
+        chars_heap = []
+        if a: heapq.heappush(chars_heap, (-a, 'a'))
+        if b: heapq.heappush(chars_heap, (-b, 'b'))
+        if c: heapq.heappush(chars_heap, (-c, 'c'))
             
+        output = []
         
-        result = []
-        
-        while count_chars:
-            fcount, first = heapq.heappop(count_chars)
+        while chars_heap:
+            max_count, max_char = heapq.heappop(chars_heap)
             
-            if len(result) >= 2 and result[-1] == result[-2] == first:
-                if not count_chars:
-                    return ''.join(result)
+            if len(output) >= 2 and output[-1] == output[-2] == max_char:
+                if not chars_heap: return ''.join(output)
                 
-                scount, second = heapq.heappop(count_chars)
-                result.append(second)
+                next_count, next_char = heapq.heappop(chars_heap)
                 
-                scount += 1
-                if scount != 0:
-                    heapq.heappush(count_chars, (scount, second))
+                output.append(next_char)
+                next_count += 1
+                
+                if next_count != 0:
+                    heapq.heappush(chars_heap,(next_count, next_char))
                     
-                heapq.heappush(count_chars, (fcount, first))
+                heapq.heappush(chars_heap,(max_count, max_char))
                 continue
                 
+            output.append(max_char)
+            max_count += 1
             
-            result.append(first)
-            fcount += 1
+            if max_count != 0:
+                heapq.heappush(chars_heap,(max_count, max_char))
+                
+        return ''.join(output)
+                
             
-            if fcount != 0: heapq.heappush(count_chars, (fcount, first))
         
-        return ''.join(result)
-                    
+        
+        

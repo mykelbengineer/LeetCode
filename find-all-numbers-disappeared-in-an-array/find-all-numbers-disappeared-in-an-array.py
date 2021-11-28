ec80f94d935bc55751573c9787d97c33dcdaacc8
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-27 17:50:15 -0800

    Time: 387 ms (29.60%), Space: 22 MB (72.88%) - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,18 +1,20 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        output = []
+        
+        # loop through the array and mark nums[i] negative to indicate visited
         for i in range(len(nums)):
             temp = abs(nums[i]) - 1
+            
             if nums[temp] > 0:
                 nums[temp] *= -1
                 
-        output = []
-        for i, k in enumerate(nums):
-            if k > 0:
-                output.append(i+1)
+        for index, value in enumerate(nums):
+            if value > 0:
+                output.append(index + 1)
+                
                 
         return output
                 
-        
-        
         
         

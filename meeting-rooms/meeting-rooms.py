a0c65d7541778c6064932d4858774fe3db032fc6
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-27 17:18:22 -0800

    Time: 68 ms (95.55%), Space: 17.4 MB (57.25%) - LeetHub

--- meeting-rooms/meeting-rooms.py
+++ meeting-rooms/meeting-rooms.py
@@ -1,21 +1,19 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         if not intervals: return True
-        sorted_intervals = sorted(intervals , key = lambda x:x[0])
-    
-        last_end_time = sorted_intervals[0][1]
+        
+        intervals.sort(key=lambda x:x[0])
+        last_meeting_end = intervals[0][1]
+        
+        
         for i in range(1, len(intervals)):
-            meeting_start = sorted_intervals[i][0]
-            
-            if meeting_start >= last_end_time:
-                last_end_time = sorted_intervals[i][1]
-                continue
-                
-            else:
+            meeting_start_time = intervals[i][0]
+
+            if last_meeting_end > meeting_start_time:
                 return False
             
+            last_meeting_end = intervals[i][1]
             
+        
         return True
-            
-            
         

102e8cd0c7e1c2a0dbfb671060ba9dba1365e23e
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-29 13:57:35 -0800

    Time: 44 ms (55.49%), Space: 14.4 MB (94.06%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,38 +1,40 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        
-        left = 0
-        right = len(nums) - 1
-        
-        while left <= right:
-            
-            mid = (left + right) // 2
-            comparator = nums[mid]
-            
-            if target < nums[0] and nums[mid] < nums[0]  or target >= nums[0] and nums[mid] >= nums[0]:
-                
-                comparator = nums[mid]
-                
-            else:
-                
-                if target < nums[0]:
-                    
-                    comparator = -inf
-                    
-                else:
-                    
-                    comparator = inf
-                    
-            if target == comparator : return mid
-            
-            if target > comparator :
-                
-                left = mid + 1
-                
-            else:
-                
-                right = mid - 1
-                
-                
-        
-        return -1
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            
+            mid = (left + right) // 2
+            comparator = nums[mid]
+            
+            if target < nums[0] and nums[mid] < nums[0]  or target >= nums[0] and nums[mid] >= nums[0]:
+                
+                comparator = nums[mid]
+                
+            else:
+                
+                if target < nums[0]:
+                    
+                    comparator = -inf
+                    
+                else:
+                    
+                    comparator = inf
+                    
+            if target == comparator : return mid
+            
+            if target > comparator :
+                
+                left = mid + 1
+                
+            else:
+                
+                right = mid - 1
+                
+                
+        
+        return -1
+        
+        

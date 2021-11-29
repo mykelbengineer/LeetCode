c3135016f5ea6ec7d4787f02d6d9eb6acc63e01d
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-29 10:05:49 -0800

    Time: 32 ms (88.01%), Space: 14.4 MB (30.63%) - LeetHub

--- rotate-image/rotate-image.py
+++ rotate-image/rotate-image.py
@@ -6,18 +6,18 @@ class Solution:
         self.transpose(matrix)
         self.reverse(matrix)
         
-        
-    
     def transpose(self, matrix):
         n = len(matrix)
+        
         for i in range(n):
-            for j in range(i,n):
-                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-        
-        
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                
     
     def reverse(self, matrix):
         n = len(matrix)
+        
         for i in range(n):
-            for j in range(n // 2):
-                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+            for j in range(n//2):
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
+        

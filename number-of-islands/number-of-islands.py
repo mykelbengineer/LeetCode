c28eab08e62c2accd4807cc20050ec0c0ff5e16e
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-30 15:28:21 -0800

    Time: 328 ms (56.73%), Space: 16.7 MB (84.72%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,26 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
-        queue = deque([])
-        
-        def bfs(queue, grid):
-            while queue:
-                I, J = queue.popleft()
-                for i, j in ([I-1, J], [I+1,J], [I, J-1], [I, J+1]):
-                    if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
-                        queue.append((i,j))
-                        grid[i][j] = '0'
-                        
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    grid[i][j] = '0'
-                    queue.append((i,j))
-                    bfs(queue, grid)
+                    self.dfs(grid, i, j)
                     count += 1
                     
-                    
-        return count 
-                    
-            
+        return count
+    
+    
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+            return 
+        
+        grid[i][j] = '#'
+        
+        self.dfs(grid, i-1,j)
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i, j-1)
+        self.dfs(grid, i, j+1)

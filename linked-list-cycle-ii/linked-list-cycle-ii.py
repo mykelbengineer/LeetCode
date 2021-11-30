5893cb61b1294460f84cc1c8427f0afc1a5851df
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-29 19:07:35 -0800

    Time: 48 ms (84.11%), Space: 17.3 MB (56.28%) - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.py
+++ linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -6,26 +6,23 @@
 
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
+        if not head: return
+        
         marker1 = marker2 = head
         
         while marker1 and marker1.next:
             marker2 = marker2.next
             marker1 = marker1.next.next
             
-            
             if marker1 == marker2:
                 break
-        else:
-            
-            return None
                 
+        else:
+            return None
         
         while head != marker1:
             head = head.next
             marker1 = marker1.next
             
-            if marker1 == head:
-                break
-                
         
         return head

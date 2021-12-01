75d6b9081d5c1c871cad04f2d0ebc69141203148
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-11-30 21:16:54 -0800

    Time: 72 ms (57.17%), Space: 15 MB (34.29%) - LeetHub

--- design-circular-queue/design-circular-queue.py
+++ design-circular-queue/design-circular-queue.py
@@ -4,43 +4,45 @@ class MyCircularQueue:
 
     def __init__(self, k: int):
         self.queue = [-1] * k
-        self.head_index = 0
+        self.head = 0
         self.count = 0
         self.capacity = k
-        self.queue_lock = Lock()
-        
+        self.queueLock = Lock()
 
     def enQueue(self, value: int) -> bool:
-        with self.queue_lock:
+        with self.queueLock:
             if self.count == self.capacity:
                 return False
-            self.queue[(self.head_index + self.count)% self.capacity] = value
+
+            self.queue[(self.head + self.count) % self.capacity] = value
             self.count += 1
-            
+        
         return True
-            
+        
 
     def deQueue(self) -> bool:
-        with self.queue_lock:
+        with self.queueLock:
             if self.count == 0:
                 return False
-            self.head_index = (self.head_index + 1) % self.capacity
+
+            self.head = (self.head + 1) % self.capacity
             self.count -= 1
-            
+        
         return True
+        
 
     def Front(self) -> int:
         if self.count == 0:
             return -1
         
-        return self.queue[self.head_index]
+        return self.queue[self.head]
         
 
     def Rear(self) -> int:
         if self.count == 0:
             return -1
         
-        return self.queue[(self.head_index + self.count - 1) % self.capacity]
+        return self.queue[(self.head + self.count - 1) % self.capacity]
         
 
     def isEmpty(self) -> bool:
@@ -48,7 +50,7 @@ class MyCircularQueue:
         
 
     def isFull(self) -> bool:
-        return self.count == self.capacity
+        return self.capacity == self.count
         
 
 

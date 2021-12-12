d096296e289dd6ff19f939a929f012f7277fef04
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-12-11 19:42:42 -0800

    Time: 234 ms (78.69%), Space: 18.9 MB (27.95%) - LeetHub

--- random-pick-with-weight/random-pick-with-weight.py
+++ random-pick-with-weight/random-pick-with-weight.py
@@ -1,34 +1,26 @@
 class Solution:
-​
-    def __init__(self, w: List[int]):
-        self.cum_sum = []
-        
-        self.cum_sum = [w[0]]
-        
-        for i in range(1, len(w)):
-            self.cum_sum.append(self.cum_sum[i-1] + w[i])
-            
-    def pickIndex(self) -> int:
-        return self.bisect(random.random() * self.cum_sum[-1], self.cum_sum)
-        
-    
-    def bisect(self, target, nums):
-        left = 0
-        right = len(nums) -1
-        
-        while left < right:
-            mid = left + (right - left) // 2
-            
-            if target > nums[mid]:
-                left = mid + 1
-                
-            else:
-                right = mid
-        
-        return left
-        
-​
-​
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(w)
-# param_1 = obj.pickIndex()
+    def __init__(self, w: List[int]):
+        """
+        :type w: List[int]
+        """
+        self.prefix_sums = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sums.append(prefix_sum)
+        self.total_sum = prefix_sum
+
+    def pickIndex(self) -> int:
+        """
+        :rtype: int
+        """
+        target = self.total_sum * random.random()
+        # run a binary search to find the target zone
+        low, high = 0, len(self.prefix_sums)
+        while low < high:
+            mid = low + (high - low) // 2
+            if target > self.prefix_sums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low

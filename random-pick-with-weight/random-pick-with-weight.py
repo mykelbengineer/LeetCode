9bede1aa5c195fef3f4f08e45fbd3136f74e900f
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-12-13 14:23:19 -0800

    Time: 252 ms (64.32%), Space: 18.7 MB (69.16%) - LeetHub

--- random-pick-with-weight/random-pick-with-weight.py
+++ random-pick-with-weight/random-pick-with-weight.py
@@ -1,26 +1,41 @@
+import random
 class Solution:
+
     def __init__(self, w: List[int]):
-        """
-        :type w: List[int]
-        """
         self.prefix_sums = []
-        prefix_sum = 0
+        self.total = 0
+        
+        total_weight_so_far = 0
         for weight in w:
-            prefix_sum += weight
-            self.prefix_sums.append(prefix_sum)
-        self.total_sum = prefix_sum
+            total_weight_so_far = weight + total_weight_so_far
+            self.prefix_sums.append(total_weight_so_far)
+            
+        self.total = total_weight_so_far
+        
 
     def pickIndex(self) -> int:
-        """
-        :rtype: int
-        """
-        target = self.total_sum * random.random()
-        # run a binary search to find the target zone
-        low, high = 0, len(self.prefix_sums)
-        while low < high:
-            mid = low + (high - low) // 2
-            if target > self.prefix_sums[mid]:
-                low = mid + 1
+        random_num = random.random()  * self.total
+        
+        left = 0
+        right = len(self.prefix_sums)
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            midVal = self.prefix_sums[mid]
+            
+            if midVal > random_num:
+                
+                right = mid
+                
             else:
-                high = mid
-        return low
+                
+                left = mid + 1
+                
+        return left
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()

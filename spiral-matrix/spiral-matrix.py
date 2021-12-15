5601b4c63a7d1d40017de1ff6cc27962236330ca
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-12-14 19:48:50 -0800

    Time: 32 ms (63.19%), Space: 14.1 MB (95.03%) - LeetHub

--- spiral-matrix/spiral-matrix.py
+++ spiral-matrix/spiral-matrix.py
@@ -1,37 +1,38 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        output = []
-        
         top = left = 0
-        bottom = len(matrix) - 1
         right = len(matrix[0]) - 1
+        bottom = len(matrix) - 1
+        output = []
         direction = 0
         
-        while (top <= bottom and left <= right):
+        while left <= right and top <= bottom:
+            
             if direction == 0:
                 for i in range(left, right+1):
                     output.append(matrix[top][i])
                     
                 top += 1
                 
-            
+                
             elif direction == 1:
                 for i in range(top, bottom+1):
                     output.append(matrix[i][right])
+                    
+                right -= 1
                 
-                right-= 1
                 
             elif direction == 2:
                 for i in range(right, left-1, -1):
                     output.append(matrix[bottom][i])
                     
-                    
                 bottom -= 1
-            
+                
+                
             else:
                 for i in range(bottom, top-1, -1):
                     output.append(matrix[i][left])
-                    
+                
                 left += 1
                 
                 
@@ -39,6 +40,6 @@ class Solution:
             
             
         return output
-        
+                    
         
         

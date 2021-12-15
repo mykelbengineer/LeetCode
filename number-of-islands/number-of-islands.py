9d5703c9e2701c8795683e04a1402baa7abb72f5
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-12-14 20:44:00 -0800

    Time: 312 ms (70.07%), Space: 16.8 MB (53.48%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -9,15 +9,18 @@ class Solution:
                     count += 1
                     
         return count
+        
+        
     
-    
-    def dfs(self, grid, i, j):
+    def dfs(self, grid, i , j):
         if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
             return 
         
-        grid[i][j] = '#'
+        grid[i][j] = '0'
         
-        self.dfs(grid, i-1,j)
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i, j-1)
-        self.dfs(grid, i, j+1)
+        
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j - 1)
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i, j + 1)
+        

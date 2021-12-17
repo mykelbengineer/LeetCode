dc8757ac5c2995a82aaebe819f58f4419a480032
mykelbengineer <51135456+mykelbengineer@users.noreply.github.com>
2021-12-16 20:56:16 -0800

    Time: 121 ms (63.76%), Space: 14.5 MB (23.62%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,18 +1,26 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = defaultdict(int)
-        res = left = maxF = 0
+        # Algorithm
+        # We count each character in our window
+        # We keep track of the max frequently occuring character
+        # If there more characters than k in our window which is not the max frequently character
+        # We reduce our window by removing s[left] from our counts
+        # Increment left
+        
+        counts = defaultdict(int)
+        left = maxF = res = 0
         
         for right in range(len(s)):
-            count[s[right]] += 1
-            maxF = max(maxF, count[s[right]])
+            counts[s[right]] += 1
+            maxF = max(maxF, counts[s[right]])
             
             while (right - left + 1) - maxF > k:
-                count[s[left]] -= 1
+                counts[s[left]] -= 1
                 left += 1
+                
                 
             res = max(res, right - left + 1)
             
-        return res
-                
             
+        return res
+        
